99c5df48d1e516b6d78c6f7841b2fe2c9ca7a7e8
melcaj13 <148561141+melcaj13@users.noreply.github.com>
2025-02-09 16:27:04 +0300

    Update fonksiyonlar.py

--- fonksiyonlar.py
+++ fonksiyonlar.py
@@ -25,11 +25,11 @@
 
 
 
-# def asal(sayi1,sayi2):
-#     for sayi in range(sayi1,sayi2+1):
-#         if sayi>1:
-#             for i in range(2,sayi):
-#                 if (sayi%i==0):
+def asal(sayi1,sayi2):
+    for sayi in range(sayi1,sayi2+1):
+        if sayi>1:
+            for i in range(2,sayi):
+                if (sayi%i==0):
 #                     break
 #                 else:
 #                     print(sayi)
