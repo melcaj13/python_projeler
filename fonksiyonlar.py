fea4ea603369d603c39d1e59705d8abf15a9a517
Sukrukrhn <Karahans149@gmail.com>
2025-02-09 16:34:40 +0300

    Update fonksiyonlar.py

--- fonksiyonlar.py
+++ fonksiyonlar.py
@@ -25,22 +25,21 @@
 
 
 
-def asal(sayi1,sayi2):
-    for sayi in range(sayi1,sayi2+1):
-        if sayi>1:
-            for i in range(2,sayi):
-                if (sayi%i==0):
+#def asal(sayi1,sayi2):
+#    for sayi in range(sayi1,sayi2+1):
+#        if sayi>1:
+#            for i in range(2,sayi):
+#                if (sayi%i==0):
 #                     break
 #                 else:
 #                     print(sayi)
 # print (asal(5,15))
 
 
-def bolen(sayi):
-    bolens=[]
-    for i in range(2,sayi):
-        if sayi%i==0:
-            bolens.append(i)
-    return bolens
-
-print(bolen(20))
+#def bolen(sayi):
+#    bolens=[]
+#    for i in range(2,sayi):
+#        if sayi%i==0:
+#            bolens.append(i)
+#    return bolens
+# print(bolen(20))
